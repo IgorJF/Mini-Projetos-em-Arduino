30c71f9b0579b86f1c9d51a6a4370aacfee41c91
IgorJF <111748228+IgorJF@users.noreply.github.com>
2024-03-04 20:13:30 -0300

    Create temperatura_giroscopio.ino

---
+++ temperatura_giroscopio.ino
@@ -0,0 +1,129 @@
+#include "DHT.h"
+#define DHTPIN 4 
+#define DHTTYPE DHT11
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_MPU6050 mpu;
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
+
+void setup() {
+  Serial.begin(115200);
+  // while (!Serial);
+  Serial.println("MPU6050 OLED demo");
+
+  if (!mpu.begin()) {
+    Serial.println("Sensor init failed");
+    while (1)
+      yield();
+  }
+  Serial.println("Found a MPU-6050 sensor");
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+  display.display();
+  delay(500); // Pause for 2 seconds
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setRotation(0);
+
+
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+void loop() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  Serial.print("Accelerometer ");
+  Serial.print("X: ");
+  Serial.print(a.acceleration.x, 1);
+  Serial.print(" m/s^2, ");
+  Serial.print("Y: ");
+  Serial.print(a.acceleration.y, 1);
+  Serial.print(" m/s^2, ");
+  Serial.print("Z: ");
+  Serial.print(a.acceleration.z, 1);
+  Serial.println(" m/s^2");
+
+  display.println("Accelerometer - m/s^2");
+  display.print(a.acceleration.x, 1);
+  display.print(", ");
+  display.print(a.acceleration.y, 1);
+  display.print(", ");
+  display.print(a.acceleration.z, 1);
+  display.println("");
+
+  Serial.print("Gyroscope ");
+  Serial.print("X: ");
+  Serial.print(g.gyro.x, 1);
+  Serial.print(" rps, ");
+  Serial.print("Y: ");
+  Serial.print(g.gyro.y, 1);
+  Serial.print(" rps, ");
+  Serial.print("Z: ");
+  Serial.print(g.gyro.z, 1);
+  Serial.println(" rps");
+
+  display.println("Gyroscope - rps");
+  display.print(g.gyro.x, 1);
+  display.print(", ");
+  display.print(g.gyro.y, 1);
+  display.print(", ");
+  display.print(g.gyro.z, 1);
+  display.println("");
+
+  //display.display();
+  delay(1000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+
+  display.println(F("Humidity: "));
+  display.print(h);
+  display.print(F("%  Temperature: "));
+  display.print(t);
+  display.print(F("°C "));
+  display.print(f);
+  display.print(F("°F  Heat index: "));
+  display.print(hic);
+  display.print(F("°C "));
+  display.print(hif);
+  display.println(F("°F"));
+
+ display.display();
+  delay(100);
+}
